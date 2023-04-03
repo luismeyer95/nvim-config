492b4ecf377e562b49fccbaf829de0d7d2c21cf4
luismeyer95 <luismeyer95@gmail.com>
2023-04-03 22:21:49 +0200

    add autotag

---
+++ lua/plugins/nvim-ts-autotag.lua
@@ -0,0 +1,15 @@
+return {
+  "windwp/nvim-ts-autotag",
+  opts = {
+    autotag = {
+      enable = true
+    }
+  },
+  ft   = {
+    'html', 'javascript', 'typescript', 'javascriptreact', 'typescriptreact', 'svelte', 'vue', 'tsx', 'jsx', 'rescript',
+    'xml',
+    'php',
+    'markdown',
+    'glimmer', 'handlebars', 'hbs'
+  }
+}
