ba4bc23316c13e3b518af7e41d93ac6c88d3ec30
luismeyer95 <luismeyer95@gmail.com>
2023-04-08 08:14:29 +0200

    fix telescope search results

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -26,14 +26,34 @@ local M = {
         },
       },
       defaults = {
-        prompt_prefix = " ⬢  ",
-        mappings = { i = { ["<esc>"] = actions.close } },
+        vimgrep_arguments = {
+          "rg",
+          "-L",
+          "--color=never",
+          "--no-heading",
+          "--with-filename",
+          "--line-number",
+          "--column",
+          "--smart-case",
+        },
+        selection_strategy = "reset",
+        sorting_strategy = "ascending",
         layout_strategy = "horizontal",
         layout_config = {
           horizontal = {
             prompt_position = "top",
+            preview_width = 0.55,
+            results_width = 0.8,
           },
+          vertical = {
+            mirror = false,
+          },
+          width = 0.87,
+          height = 0.80,
+          preview_cutoff = 120,
         },
+        prompt_prefix = " ⬢  ",
+        mappings = { i = { ["<esc>"] = actions.close } },
       },
       extensions = {
         fzf = {
