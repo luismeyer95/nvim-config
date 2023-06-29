d02c26dfda05ccde7c51428a444678b4de199cf1
luismeyer95 <luismeyer95@gmail.com>
2023-06-30 00:04:57 +0200

    Clean up diffview config + update

--- lua/plugins/diffview.lua
+++ lua/plugins/diffview.lua
@@ -1,20 +1,5 @@
 return {
-    "sindrets/diffview.nvim",
-    dependencies = "nvim-lua/plenary.nvim",
-    init = function()
-        -- load gitsigns only when a git file is opened
-        vim.api.nvim_create_autocmd({ "BufRead" }, {
-            group = vim.api.nvim_create_augroup("DiffviewLazyLoad", { clear = true }),
-            callback = function()
-                vim.fn.system("git -C " .. vim.fn.expand "%:p:h" .. " rev-parse")
-                if vim.v.shell_error == 0 then
-                    vim.api.nvim_del_augroup_by_name "DiffviewLazyLoad"
-                    vim.schedule(function()
-                        ---@diagnostic disable-next-line: different-requires
-                        require("lazy").load { plugins = { "diffview.nvim" } }
-                    end)
-                end
-            end,
-        })
-    end,
+  "sindrets/diffview.nvim",
+  dependencies = "nvim-lua/plenary.nvim",
+  lazy = false,
 }
