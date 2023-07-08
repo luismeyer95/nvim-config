80d62391815302e34dbdefeee3dc6529643ff2f6
luismeyer95 <luismeyer95@gmail.com>
2023-07-08 17:33:06 +0200

    Fix nvim-surround/flash conflict on keybinding

--- lua/plugins/nvim-surround.lua
+++ lua/plugins/nvim-surround.lua
@@ -1,10 +1,13 @@
 return {
   "kylechui/nvim-surround",
   lazy = false,
-  version = "*",   -- Use for stability; omit to use `main` branch for the latest features
-  config = function()
-    require("nvim-surround").setup {
-      -- Configuration here, or leave empty to use defaults
-    }
-  end,
+  version = "*", -- Use for stability; omit to use `main` branch for the latest features
+  opts = {
+    keymaps = {
+      normal_cur = false,
+      normal_line = false,
+      normal_cur_line = false,
+      visual_line = false,
+    },
+  }
 }
