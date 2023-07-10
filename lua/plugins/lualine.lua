3533cbc671775fe4b0c4d0e802c53ece007db681
luismeyer95 <luismeyer95@gmail.com>
2023-07-10 09:02:56 +0200

    simplify lualine config

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,21 +1,19 @@
 return {
   'nvim-lualine/lualine.nvim',
   dependencies = 'nvim-tree/nvim-web-devicons',
-  event = "VimEnter",
-  config = function()
-    require("lualine").setup {
-      sections = {
-        lualine_c = {
-          "filename",
-          {
-            function()
-              local key = require("grapple").key()
-              return "  [" .. key .. "]"
-            end,
-            cond = require("grapple").exists,
-          }
+  lazy = false,
+  opts = {
+    sections = {
+      lualine_c = {
+        "filename",
+        {
+          function()
+            local key = require("grapple").key()
+            return "  [" .. key .. "]"
+          end,
+          cond = require("grapple").exists,
         }
       }
     }
-  end
+  },
 }
