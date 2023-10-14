57a5fe7df5c8fd28cbbf6bd54be692f06759684b
luismeyer95 <luismeyer95@gmail.com>
2023-10-14 22:31:12 +0200

    add new gpt session client

---
+++ lua/plugins/chatgpt.lua
@@ -0,0 +1,22 @@
+return {
+  "jackMort/ChatGPT.nvim",
+  event = "VeryLazy",
+  config = function()
+    require("chatgpt").setup({
+      openai_params = {
+        model = "gpt-4",
+        frequency_penalty = 0,
+        presence_penalty = 0,
+        max_tokens = 300,
+        temperature = 0,
+        top_p = 1,
+        n = 1,
+      },
+    })
+  end,
+  dependencies = {
+    "MunifTanjim/nui.nvim",
+    "nvim-lua/plenary.nvim",
+    "nvim-telescope/telescope.nvim"
+  }
+}
